0b7ccfde5d05397bac27ae89fb85310fe92cc441
gouqingsong <gouqingsong@noboauto.com>
2021-05-19 11:42:40 +0800

    fix btn and backlight dev

--- app/src/backlight.c
+++ app/src/backlight.c
@@ -1,3 +1,6 @@
+#include <stdbool.h>
+#include <drivers/gpio.h>
+
 #include "backlight.h"
 
 static const struct device* backlight_dev;
@@ -8,13 +11,15 @@ static const struct device* backlight_dev;
 #define BACKLIGHT_2     DT_GPIO_PIN(DT_ALIAS(led1), gpios)
 #define BACKLIGHT_3     DT_GPIO_PIN(DT_ALIAS(led2), gpios)
 
-int backlight_init()
+int backlight_init(const struct device *dev)
 {
     backlight_dev = device_get_binding(BACKLIGHT_PORT);
     gpio_pin_configure(backlight_dev, BACKLIGHT_1, GPIO_OUTPUT);
     gpio_pin_configure(backlight_dev, BACKLIGHT_2, GPIO_OUTPUT);
     gpio_pin_configure(backlight_dev, BACKLIGHT_3, GPIO_OUTPUT);
     backlight_enable(true);
+
+    return 0;
 }
 
 int backlight_enable(bool enable)
@@ -23,3 +28,5 @@ int backlight_enable(bool enable)
     gpio_pin_set_raw(backlight_dev, BACKLIGHT_2, enable ? 0 : 1);
     gpio_pin_set_raw(backlight_dev, BACKLIGHT_3, enable ? 0 : 1);
 }
+
+SYS_INIT(backlight_init, APPLICATION, CONFIG_KERNEL_INIT_PRIORITY_DEFAULT);
--- app/src/btn_event.c
+++ app/src/btn_event.c
@@ -1,26 +1,36 @@
 #include <zephyr.h>
 #include <drivers/gpio.h>
 
+
+#define BTN_PORT    DT_GPIO_LABEL(DT_ALIAS(sw0), gpios)
+
+#define KEY_IN      DT_GPIO_PIN(DT_ALIAS(sw0), gpios)
+#define KEY_OUT     DT_GPIO_PIN(DT_ALIAS(sw1), gpios)
+
+#define BTN_DEBOUNCE_TIME 200 /* 200 ms */
+
 static struct k_timer timer_debounce;
 
-K_MSGQ_DEFINE(evt_msgq, sizeof(struct msg), 10, 4);
 
-static sys_slist_t evt_list;
+// K_MSGQ_DEFINE(evt_msgq, sizeof(struct msg), 10, 4);
+
+//static sys_slist_t evt_list;
+static struct gpio_callback button_cb;
 
 struct node {
     int (*func)(void *arg);
-    struct sys_slist_t evt_list;
+    //struct sys_slist_t evt_list;
 };
 
 void button_event_send(struct k_timer *timer)
 {
     // send btn event
-    k_msg_put();
+    // k_msg_put();
 }
 
-static void button_event_cb(struct device *dev, struct gpio_callback *cb, uint32_t pins)
+static void button_event_cb(const struct device *dev, struct gpio_callback *cb, uint32_t pins)
 {
-    k_timer_start(&timer_debounce, K_NO_WAIT, K_SECONDS(BTN_DEBOUNCE_TIME));
+    k_timer_start(&timer_debounce, K_NO_WAIT, K_MSEC(BTN_DEBOUNCE_TIME));
 }
 
 int btn_event_init(const struct device *dev)
@@ -28,14 +38,18 @@ int btn_event_init(const struct device *dev)
     int retval = 0;
     k_timer_init(&timer_debounce, button_event_send, NULL);
 
-    struct device *button_dev = device_get_binding("gpio");
+    const struct device *button_dev = device_get_binding(BTN_PORT);
     if (!button_dev) {
-        return -1;
+        return -ENODEV;
     }
 
-    gpio_pin_configure(button_dev, BTN_IN, GPIO_INPUT | GPIO_INT_EDGE_FALLING | PULL_UP);
+    /* Set button out pin to high to enable the button */
+    gpio_pin_configure(button_dev, KEY_OUT, GPIO_OUTPUT);
+    gpio_pin_set_raw(button_dev, KEY_OUT, 1U);
 
-    gpio_init_callback(&button_cb, button_event_cb, BIT(BTN_IN));
+    gpio_pin_configure(button_dev, KEY_IN, GPIO_INPUT | GPIO_INT_EDGE_FALLING/* | PULL_UP*/);
+
+    gpio_init_callback(&button_cb, button_event_cb, BIT(KEY_IN));
     gpio_add_callback(button_dev, &button_cb);
 
     return retval;
