e2fee355ecc115fd6fd21264c66d73b4ecd9f4ef
gouqingsong <gouqingsong@noboauto.com>
2021-07-30 20:02:05 +0800

    fix vibrator pwm

--- app/src/service/vibrator.c
+++ app/src/service/vibrator.c
@@ -1,10 +1,26 @@
 #include <zephyr.h>
+#include <device.h>
+#include <drivers/pwm.h>
 
 #include "vibrator.h"
 
-#define VIRBRATOR_STACK_SIZE    1024
+#define VIBRATOR_STACK_SIZE    1024
 #define VIBRATOR_PRIORITY       6
 
+#define PERIOD_USEC	(20U * USEC_PER_MSEC) /* 20ms */
+
+#define PWM_MOTOR_NODE DT_ALIAS(pwm_motor)
+
+#if DT_NODE_HAS_STATUS(PWM_MOTOR_NODE, okay)
+#define PWM_LABEL	DT_PWMS_LABEL(PWM_MOTOR_NODE)
+#define PWM_CHANNEL	DT_PWMS_CHANNEL(PWM_MOTOR_NODE)
+#define PWM_FLAGS	DT_PWMS_FLAGS(PWM_MOTOR_NODE)
+#else
+#error "Unsupported board: pwm-motor0 devicetree alias is not defined"
+#define PWM_LABEL	""
+#define PWM_CHANNEL	0
+#define PWM_FLAGS	0
+#endif
 
 K_MSGQ_DEFINE(vibrator_q, sizeof(const struct vibrate_pattern *), 8, 4);
 
@@ -68,16 +84,16 @@ static const struct vibrate_pattern default_vibrate_patterns[VIBRATOR_CMD_MAX] =
     }
 };
 
-static int virbrator_send_command(const struct vibrate_pattern *pattern)
+static int vibrator_send_command(const struct vibrate_pattern *pattern)
 {
     const struct vibrate_pattern *p = pattern;
     return k_msgq_put(&vibrator_q, &p, K_FOREVER);
 }
 
-void virbrator_command(enum vibrator_cmd cmd)
+void vibrator_command(enum vibrator_cmd cmd)
 {
     if (cmd < VIBRATOR_CMD_MAX) {
-        virbrator_send_command(&default_vibrate_patterns[cmd]);
+        vibrator_send_command(&default_vibrate_patterns[cmd]);
     } else {
     }
 }
@@ -87,14 +103,23 @@ void vibrator_play_pattern(const struct vibrate_pattern *pattern)
 
 }
 /*
-virbrator_enable()
-virbrator_disable()
+vibrator_enable()
+vibrator_disable()
 */
-static void virbrator_thread()
+static void vibrator_thread()
 {
     uint8_t buf_idx = 0;
     uint8_t idx = 0;
+
+    const struct device *pwm_dev = NULL;
+    pwm_dev = device_get_binding(PWM_LABEL);
+    if (!pwm_dev) {
+        printk("Error: didn't find %s device\n", PWM_LABEL);
+        return;
+    }
+    pwm_pin_set_usec(pwm_dev, PWM_CHANNEL, PERIOD_USEC, 700, PWM_FLAGS);
     static const struct vibrate_pattern *current_pattern = &default_vibrate_patterns[VIBRATE_CMD_STOP];
+    printk("vibrator init done\n");
     for (; ;) {
         if (k_msgq_get(&vibrator_q, &current_pattern, K_FOREVER))
             continue;
@@ -113,6 +138,6 @@ static void virbrator_thread()
         current_pattern = &default_vibrate_patterns[VIBRATE_CMD_STOP];
     }
 }
-K_THREAD_DEFINE(virbrator, VIRBRATOR_STACK_SIZE, virbrator_thread,
+K_THREAD_DEFINE(vibrator, VIBRATOR_STACK_SIZE, vibrator_thread,
                 NULL, NULL, NULL,
                 VIBRATOR_PRIORITY, 0, 0);
