dfe04f18cd85d1a040c7ac997558aadda50103fe
gouqingsong <gouqingsong@noboauto.com>
2021-08-04 17:29:50 +0800

    fix charger module

--- app/src/charger.c
+++ app/src/charger.c
@@ -1,25 +1,31 @@
 #include <zephyr.h>
+#include <device.h>
+#include <drivers/gpio.h>
+#include <logging/log.h>
 
-#define CHARGER_PIN     12
+#define CHARGER_PIN     DT_GPIO_PIN(DT_ALIAS(charger), gpios)
+
+LOG_MODULE_REGISTER(CHARGER, LOG_LEVEL_INF);
 
 static const struct device *charging_dev;
 static struct gpio_callback charging_cb;
 
-static void battery_charging_isr(const struct device *gpiobat, struct gpio_callback *cb, uint32_t pins)
+static void battery_charging_isr(const struct device *dev, struct gpio_callback *cb, uint32_t pins)
 {
-//    uint32_t res = gpio_pin_get(charging_dev, BAT_CHA);
+    int val = gpio_pin_get(dev, CHARGER_PIN);
+    LOG_INF("val = %d", val);
 //	battery_update_charging_status(res != 1U);
 }
 
 int charger_init(const struct device *dev)
 {
-    charging_dev = device_get_binding("GPIO_0");
-    if (charging_dev ==NULL) {
+    charging_dev = device_get_binding(DT_GPIO_LABEL(DT_ALIAS(charger), gpios));
+    if (charging_dev == NULL) {
         return -ENODEV;
     }
 
-    gpio_pin_configure(charging_dev, CHARGER_PIN, GPIO_INPUT | GPIO_INT_EDGE_BOTH);
-    gpio_init_callback(&charging_cb, battery_charging_isr, BIT(BAT_CHA));
+    gpio_pin_configure(charging_dev, CHARGER_PIN, GPIO_INPUT | GPIO_INT_EDGE_FALLING | DT_GPIO_FLAGS(DT_ALIAS(charger), gpios));
+    gpio_init_callback(&charging_cb, battery_charging_isr, BIT(CHARGER_PIN));
     gpio_add_callback(charging_dev, &charging_cb);
 }
 
