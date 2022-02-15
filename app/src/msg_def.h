7e73fe35a743975764ed953f57a5b4e006bc14a4
Qingsong Gou <gouqs@hotmail.com>
2022-02-15 09:41:30 +0800

    add export func

--- app/src/msg_def.h
+++ app/src/msg_def.h
@@ -1,6 +1,8 @@
 #ifndef _MSG_DEF_H
 #define _MSG_DEF_H
 
+#include <zephyr.h>
+
 enum msg_type {
 	MSG_TYPE_BLE_CONNECTED,
 	MSG_TYPE_BLE_DISCONNECTED,
@@ -16,5 +18,10 @@ struct msg_ble_conn {
 	int err;
 	struct bt_conn *conn;
 };
+
+
+void sys_push_msg(uint32_t msg_type);
+
+void sys_push_msg_with_data(uint32_t msg_type, void *data, size_t size);
 
 #endif /* _MSG_TYPE_H */
