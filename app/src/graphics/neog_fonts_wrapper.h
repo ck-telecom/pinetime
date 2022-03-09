51d3c9ca532d386bffca387fd5055f91cf9846d5
Qingsong Gou <gouqs@hotmail.com>
2022-03-09 15:22:49 +0800

    Revert "graphics: initial commit"
    This reverts commit 6f4a175b7a265d71b66e8b7d35b82549908872ff.

--- app/src/graphics/neog_fonts_wrapper.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-#include "graphics.h"
-
-#define fonts_get_system_font n_fonts_get_system_font
-#define fonts_load_custom_font n_fonts_load_custom_font
-#define fonts_unload_custom_font n_fonts_unload_custom_font
