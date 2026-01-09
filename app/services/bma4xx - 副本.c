aa97a7b4569a951857263b6982ca802750f37dd0
Qingsong Gou <gouqs@hotmail.com>
2026-01-09 20:49:56 +0800

    add more dirs
    Signed-off-by: Qingsong Gou <gouqs@hotmail.com>

---
+++ app/services/bma4xx - 副本.c
@@ -0,0 +1,93 @@
+#include <zephyr.h>
+#include <device.h>
+#include <drivers/sensor.h>
+#include <logging/log.h>
+#include <math.h>
+
+#include "bma4.h"
+#include "display_api.h"
+#include "../event/event_service.h"
+
+#define BMA4XX_STACK_SIZE   1024
+#define BMA4XX_PRIORITY     5
+
+LOG_MODULE_REGISTER(bma4xx, LOG_LEVEL_INF);
+
+#ifdef CONFIG_BMA421_TRIGGER
+static void bma421_handler(const struct device *dev,
+			struct sensor_trigger *trig)
+{
+    struct sensor_value val[5];
+
+    if (sensor_sample_fetch(dev) < 0) {
+        LOG_ERR("sensor_sample_fetch error");
+        return;
+    }
+
+	if (sensor_channel_get(dev, SENSOR_CHAN_ALL, val) < 0) {
+        LOG_ERR("sensor_channel_get error");
+        return;
+    }
+    LOG_INF("bma421 handler");
+}
+#endif
+
+static void bma4xx_thread()
+{
+    struct sensor_value accel_data[5] = { 0 };
+    struct sensor_value ori[3] = { 0 };
+    double orientation[3] = { 0 };
+
+    const struct device *dev = device_get_binding("BMA421");
+    if (dev == NULL) {
+        LOG_ERR("could not get BMA421 device");
+        return;
+    }
+
+    if (IS_ENABLED(CONFIG_BMA421_TRIGGER)) {
+        struct sensor_trigger trig = {
+            .type = SENSOR_TRIG_DATA_READY,
+            .chan = SENSOR_CHAN_ALL,
+        };
+        if (sensor_trigger_set(dev, &trig, bma421_handler) < 0) {
+            LOG_ERR("Cannot configure trigger");
+            return;
+        }
+    }
+
+    while (1) {
+        if (sensor_sample_fetch(dev) < 0) {
+            LOG_ERR("sensor_sample_fetch error");
+        }
+        if (sensor_channel_get(dev, SENSOR_CHAN_ACCEL_XYZ, accel_data) < 0) {
+            LOG_ERR("sensor_channel_get error");
+        }
+        double x = sensor_value_to_double(&accel_data[0]);
+        double y = sensor_value_to_double(&accel_data[1]);
+        double z = sensor_value_to_double(&accel_data[2]);
+
+        msg_send_data(EventServiceCommandGeneric, sizeof(accel_data), accel_data);
+
+        LOG_INF("sensor_channel_get accel.X %d.%d", accel_data[0].val1, accel_data[0].val2);
+        LOG_INF("sensor_channel_get accel.Y %d.%d", accel_data[1].val1, accel_data[1].val2);
+        LOG_INF("sensor_channel_get accel.Z %d.%d", accel_data[2].val1, accel_data[2].val2);
+        LOG_INF("steps: %d", accel_data[4].val1);
+        orientation[0] = (float) atan2(x, sqrt(y * y + z * z));
+        orientation[1] = (float) atan2(y, sqrt(x * x + z * z));
+        orientation[2] = (float) atan2(x, sqrt(y * y + x * x));
+
+        sensor_value_from_double(&ori[0], orientation[0]);
+        sensor_value_from_double(&ori[1], orientation[1]);
+        sensor_value_from_double(&ori[2], orientation[2]);
+
+        int32_t pitch = sensor_rad_to_degrees(&ori[0]);
+        int32_t roll  = sensor_rad_to_degrees(&ori[1]);
+        int32_t yaw   = sensor_rad_to_degrees(&ori[2]);
+
+        LOG_INF("pitch %d, roll %d, yaw %d", pitch, roll, yaw);
+
+        k_sleep(K_MSEC(500));
+    }
+}
+
+K_THREAD_DEFINE(bma4xx_tid, BMA4XX_STACK_SIZE, bma4xx_thread, NULL, NULL, NULL, BMA4XX_PRIORITY, 0, 0);
---
+++ app/services/touch - 副本.c
@@ -0,0 +1,158 @@
+#include <zephyr.h>
+#include <device.h>
+#include <drivers/sensor.h>
+#include <logging/log.h>
+#include <lvgl.h>
+#include "cst816s.h"
+#include "display_api.h"
+
+#define TOUCH_STACK_SIZE    1024
+#define TOUCH_PRIORITY      5
+
+#define TOUCH_DEV "CST816S"
+
+LOG_MODULE_REGISTER(touch, LOG_LEVEL_INF);
+
+K_MSGQ_DEFINE(kscan_msgq, sizeof(lv_indev_data_t),
+          16, 4);
+
+static bool lvgl_indev_read_cb(lv_indev_drv_t *drv, lv_indev_data_t *data)
+{
+#if 0
+    lv_indev_data_t curr;
+
+    static lv_indev_data_t prev = {
+        .point.x = 0,
+        .point.y = 0,
+        .state = LV_INDEV_STATE_REL,
+    };
+
+    prev.state = LV_INDEV_STATE_REL;
+    if (k_msgq_get(&kscan_msgq, &curr, K_NO_WAIT) != 0) {
+        goto set_and_release;
+    }
+
+    prev = curr;
+
+set_and_release:
+    *data = prev;
+
+    return k_msgq_num_used_get(&kscan_msgq) > 0;
+#else
+    const struct device *dev = drv->user_data;
+LOG_INF("%s", __func__);
+    if (sensor_sample_fetch(dev) < 0) {
+        LOG_ERR("Touch sample update error.");
+    }
+
+    struct sensor_value state;
+    struct sensor_value touch_point;
+
+    sensor_channel_get(dev, CST816S_CHAN_TOUCH_POINT_1, &touch_point);
+
+    sensor_channel_get(dev, CST816S_CHAN_STATE, &state);
+
+    data->point.x = touch_point.val1;
+    data->point.y = touch_point.val2;
+    data->state = (state.val1 == UP) ? LV_INDEV_STATE_REL : LV_INDEV_STATE_PR;
+
+    return false;
+#endif
+}
+
+static int lvgl_indev_init(void *dev)
+{
+    lv_indev_drv_t indev_drv;
+
+    lv_indev_drv_init(&indev_drv);
+    indev_drv.type = LV_INDEV_TYPE_POINTER;
+    indev_drv.read_cb = lvgl_indev_read_cb;
+    indev_drv.user_data = (void *)dev;
+
+    if (lv_indev_drv_register(&indev_drv) == NULL) {
+        LOG_ERR("Failed to register input device.");
+        return -EPERM;
+    }
+
+    return 0;
+}
+
+void touch_isr_handler(const struct device *touch_dev, struct sensor_trigger *tap)
+{
+#if 0
+    enum cst816s_gesture gesture;
+    struct sensor_value touch_point;
+
+    if (sensor_sample_fetch(touch_dev) < 0) {
+        LOG_ERR("Touch sample update error.");
+    }
+
+//    display_wake_up();
+//    backlight_enable(true);
+//    k_timer_start(&display_off_timer, DISPLAY_TIMEOUT, K_NO_WAIT);
+
+    struct sensor_value gesture_val;
+    sensor_channel_get(touch_dev, CST816S_CHAN_GESTURE, &gesture_val);
+    gesture = gesture_val.val1;
+    sensor_channel_get(touch_dev, CST816S_CHAN_TOUCH_POINT_1, &touch_point);
+    //LOG_INF("Gesture %d on x=%d, y=%d", gesture, touch_point.val1, touch_point.val2);
+    if (gesture == CLICK) {/*
+        lv_indev_data_t data = {
+            .point.x = touch_point.val1,
+            .point.y = touch_point.val2,
+            .state = LV_INDEV_STATE_PR,
+        };
+        LOG_INF("Gesture %d on x=%d, y=%d", gesture, touch_point.val1, touch_point.val2);
+        if (k_msgq_put(&kscan_msgq, &data, K_NO_WAIT) != 0) {
+            LOG_ERR("Could put input data into queue");
+        }*/
+    } else {
+        msg_send_gesture(gesture);
+        LOG_DBG("Gesture %d", gesture);
+    }
+#endif
+}
+
+int touch_init(const struct device *dev)
+{
+
+#if 0
+    if (IS_ENABLED(CONFIG_CST816S_TRIGGER)) {
+        struct sensor_trigger trig = {
+            .type = SENSOR_TRIG_DATA_READY,
+            .chan = SENSOR_CHAN_ACCEL_XYZ,
+        };
+        if (sensor_trigger_set(touch_dev, &trig, touch_isr_handler) < 0) {
+            LOG_ERR("Cannot configure trigger");
+            return -ENOTSUP;
+        }
+    }
+#endif
+    return 0;//lvgl_indev_init((void *)touch_dev);
+}
+SYS_INIT(touch_init, APPLICATION, 91);
+
+static void touch_thread()
+{
+    const struct device *dev = device_get_binding(TOUCH_DEV);
+    if (!dev) {
+        LOG_ERR("could get %s device", TOUCH_DEV);
+        return -ENODEV;
+    }
+    while (1) {
+        k_sleep(K_MSEC(100));
+
+        if (sensor_sample_fetch(dev) < 0) {
+            LOG_ERR("Touch sample update error.");
+        }
+
+        struct sensor_value state;
+        struct sensor_value touch_point;
+
+        sensor_channel_get(dev, CST816S_CHAN_TOUCH_POINT_1, &touch_point);
+
+        sensor_channel_get(dev, CST816S_CHAN_STATE, &state);
+        LOG_INF("Gesture %d on x=%d, y=%d", state.val1, touch_point.val1, touch_point.val2);
+    }
+}
+K_THREAD_DEFINE(touch_tid, TOUCH_STACK_SIZE, touch_thread, NULL, NULL, NULL, TOUCH_PRIORITY, 0, 0);
